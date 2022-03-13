6712f15ffcb2cbe49c5812085b5aaf1cc7078a95
Antonio Fasano <AntonioFasano@users.noreply.github.com>
2022-03-13 23:00:23 +0100

    bloomr.download.build.R now based on R curl and builds in CWD

--- bloomr.download.build.R
+++ bloomr.download.build.R
@@ -1,36 +1,61 @@
-## Download 'bloomr.build.R' from Gthub and build it inside 'scriptdir/data'
+## Download 'bloomr.build.R' from Github and ask to build it inside a date-based dir in the cwd
 ## Intended for fast internal testing - Readme method is the standard way
-## Use it by drag and drop in R GUI
+
+## script.path= parent.frame(2)$ofile
+
+loadLib <- function(lib){
+    if (!lib %in% rownames(installed.packages())){
+        repo <- getOption("repos")[["CRAN"]]
+        if(repo=="@CRAN@") repo <- "http://cran.r-project.org"
+        install.packages(lib, repos=repo)
+    }
+    if(!eval(parse(text=paste('require(', lib,')')))){
+        message("\nUnable to load", lib, "library")
+        return (FALSE)
+    }
+
+    if(!has_internet()){
+        message("\nThere is no internet connection.")
+        return (FALSE)
+    } 
+    
+    return (TRUE)
+}
 
 source_github <- function(u) {
-    ## load package
-    if(!require(RCurl)) {
-        install.packages( "RCurl"  , repos= "http://cran.r-project.org/") 
-        require(RCurl)
+
+    req <- curl_fetch_memory(u)
+    if(req$status_code >= 400){
+        message("\The download of the build script from GitHub failed with status code ", req$status_code)
+        return (FALSE)
+    } else {
+        script <- rawToChar(req$content)
+        script <- gsub("\\r", "", script) # remove unix line endings
+        ## message( script )    
+        eval(parse(text = script), envir=.GlobalEnv)
+        return(TRUE)
     }
+}
+
+
+main <- function(){
+
+    buildscript <- "https://raw.githubusercontent.com/AntonioFasano/BloomR/master/bloomr.build.R"
     
+    ## Load curl package
+    if(!loadLib("curl")) return(1)
 
-    ## read script lines from website and evaluate
-    script <- getURL(u, ssl.verifypeer = FALSE)
+    ## Load build script
+    if(!(source_github(buildscript))) return(1)
 
-    script =gsub("\\r", "", script) # unix line endings
-    eval(parse(text = script),envir=.GlobalEnv)
-}  
+    ## Create download path
+    dirname <- format(Sys.time(), "bloomr-%y%m%d-%H%M")
+    dirpath <- file.path(getwd(), dirname)
+    
+    ## Start download and build
+    ans <- readline(sprintf("I will create BloomeR in %s . (Y/n)? ", dirpath))
+    if(tolower(ans)=="y" || ans=="")  makeBloomR(dirpath) else print('bye')
 
-
-### Set outdir
-##cdir=getwd()
-script.path= parent.frame(2)$ofile
-bdir= paste0(dirname(script.path), format(Sys.time(), "/%y%m%d-%H%M"))
-
-
-### Start download and build
-ans <- readline(sprintf("I will create BloomeR in %s . (Y/n)? ", bdir))
-if(tolower(ans)=="y" || ans==""){
-    source_github(
-      "https://raw.githubusercontent.com/AntonioFasano/BloomR/master/bloomr.build.R"
-        )
-    makeBloomR(bdir)
-} else print('bye') 
-
+}
  
+main()
